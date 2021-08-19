3875b3cd60cd14aa73711d626f4daf89fd2a25bf
Anukash26 <85529302+Anukash26@users.noreply.github.com>
2021-08-19 17:51:45 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var runner, boy
+var path, pathImage
+
+
+function preload(){
+  //pre-load images
+  pathImage = loadImage("path.png");
+  runner = loadAnimation("Runner-1.png","Runner-2.png"); 
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  path=createSprite(200,200);
+  path.addImage(pathImage);
+  path.velocityY =4;
+  path.scale=1.2;
+  //code to reset background 
+  if(path.y>400){
+    path.y=height/2;
+
+    }
+  boy = createSprite(200,200);
+  boy.addAnimation("running", runner)
+  boy.scale=0.05
+  boy.y = 350;
+
+  leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false;
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+}
+
+
+
+function draw() {
+  background(0);
+
+  boy.x=World.mouseX;
+   
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+ 
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+
+
+ drawSprites();
+}
